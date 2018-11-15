fcb844900d7832753a8452105c0f6ed98403c603
Jarno Virta <jarnovirta@users.noreply.github.com>
2018-11-15 22:29:50 +0200

    Finished 3.9

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const morgan = require('morgan')
 
 app.use(cors())
 app.use(bodyParser.json())
+app.use(morgan(':method :url :reqBody :status :res[content-length] - :response-time ms'))
+
+morgan.token('reqBody', (req, res) => JSON.stringify(req.body))
 
 const persons = [
   {
