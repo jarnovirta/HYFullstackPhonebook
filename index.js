7c73b49a3edfa945ade9407f620c9446a38cb8eb
Jarno Virta <jarnovirta@users.noreply.github.com>
2018-11-15 23:38:40 +0200

    assignment 3.9

--- index.js
+++ index.js
@@ -37,27 +37,40 @@ app.get('/api/persons/:id', (req, res) => {
 app.post('/api/persons', (req, res) => {
   const newPerson = req.body
   if (Object.keys(req.body).length === 0) {
+    console.log("body")
     return res.status(400).send({ error: 'Empty request body'})
   }
   if (newPerson.name === undefined || newPerson.number === undefined) {
+    console.log("name or number")
     return res.status(400).send({ error: 'Name or number missing'})
   }  
-  if (persons.filter(person => person.name === newPerson.name)) {
+  if (persons.filter(person => person.name === newPerson.name).length > 0) {
+    console.log("exists")
     return res.status(400).send({ error: 'Name must be unique'})
   }
   newPerson.id = Math.floor(Math.random() * 10000 + 1)
-  persons.concat(newPerson) 
+  persons.push(newPerson) 
   res.json(newPerson)
 })
 app.delete('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id)
   const person = persons.find(person => person.id === id)
   if (person) {
-    persons.splice(persons.indexOf(person), persons.indexOf(person) + 1)
+    let index = persons.indexOf(person)
+    persons.splice(index, index + 1)
   }
   res.sendStatus(204)
 })
-
+app.put('/api/persons/:id', (req, res) => {
+  const newPerson = req.body
+  const id = Number(req.params.id)
+  const existingPerson = persons.find(person => person.id === id)
+  if (!existingPerson) return res.status(404).send({ error: 'Record not found' })
+  newPerson.id = existingPerson.id
+  let index = persons.indexOf(existingPerson)
+  persons.splice(index, 1, newPerson)
+  res.json(newPerson)
+})
 app.get('/info', (req, res) => {
   res.set({
     'Content-Type': 'text/plain;characterEncoding=UTF-8'
@@ -65,7 +78,6 @@ app.get('/info', (req, res) => {
   res.end('Puhelinluettelossa on ' + persons.length + ' henkilön tiedot'
     + '\n\n' + new Date())
 })
-
 
 const PORT = 3001
 app.listen(PORT, () => {
